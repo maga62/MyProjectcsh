8a2cd56b6f62190df5bce2270c92ceb53973a0b3
maga62 <muhammad709059952@gmail.com>
2021-05-05 07:40:22 +0300

    API demi?

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //attribute
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency Chain ---
+            IProductService productService = new ProductManager(new EfProductDal());
+            var result = productService.GetAll();
+            return result.Data;
+
+        }
+    }
+}
